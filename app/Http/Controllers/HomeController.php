f09bf2a7ff6d3f5bd810d263346d1824754bddb1
Miguelo-hub <geniszully@gmail.com>
2026-05-14 15:26:23 -0500

    index corregido

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,19 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Proyecto;
 use App\Models\Empresa;
 use App\Models\Aprendiz;
+use App\Models\Instructor;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $totalProyectos = Proyecto::activos()->count();
+        $totalProyectos = Proyecto::whereIn('estado', ['aprobado', 'en_progreso'])->count();
         $totalEmpresas = Empresa::where('activo', 1)->count();
-        $totalAprendices = Aprendiz::activos()->count();
+        $totalAprendices = Aprendiz::where('activo', true)->count();
+        $totalInstructores = Instructor::where('activo', true)->count();
 
         return view('index', compact(
             'totalProyectos',
             'totalEmpresas',
-            'totalAprendices'
+            'totalAprendices',
+            'totalInstructores'
         ));
     }
 }
